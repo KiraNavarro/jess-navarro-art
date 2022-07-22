f7f0cd411347e0c7da8fd21715e23eb2aa0d2607
Jessica Navarro <kira.navarro@gmail.com>
2022-07-22 14:49:54 -0700

    Add CSS Modules

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,17 +1,8 @@
 import * as React from "react"
 import { Link } from 'gatsby'
+import * as styles from './layout.module.css'
 
 // styles
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
 const headingAccentStyles = {
   color: "#663399",
 }
@@ -79,67 +70,24 @@ const badgeStyle = {
   lineHeight: 1,
 }
 
-// data
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
-
 // markup
 const Layout = ( { pageTitle, pageHeading, children }) => {
   return (
-    <main style={pageStyles}>
+    <main className={styles.pageStyles}>
       <title>{pageTitle}</title>
-      <h1 style={headingStyles}>
-        {pageHeading}
-      </h1>
       <nav>
-        <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">About</Link></li>
+        <ul className={styles.navLinks}>
+            <li className={styles.navLinkItem}>
+                <Link to="/" className={styles.navLinkText}>Home</Link>
+            </li>
+            <li className={styles.navLinkItem}>
+                <Link to="/about" className={styles.navLinkText}>About</Link>
+            </li>
         </ul>
       </nav>
+      <h1 className={styles.headingStyles}>
+        {pageHeading}
+      </h1>
       {children}
       <img
         alt="Gatsby G Logo"
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,16 +2,6 @@ import * as React from "react"
 import Layout from "../components/layout"
 
 // styles
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
 const headingAccentStyles = {
   color: "#663399",
 }
